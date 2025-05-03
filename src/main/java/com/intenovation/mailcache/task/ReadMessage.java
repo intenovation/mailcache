195f57ba4b2ce1041dea0aee9a6303be8d2eed70
jens <jens.meise@me.com>
2025-05-03 15:17:28 -0700

    refactoring

---
+++ src/main/java/com/intenovation/mailcache/task/ReadMessage.java
@@ -0,0 +1,31 @@
+package com.intenovation.mailcache.task;
+
+import com.intenovation.appfw.task.ProgressStatusCallback;
+import com.intenovation.mailcache.CachedFolder;
+import com.intenovation.mailcache.CachedMessage;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+
+/**
+ * Task that reads a message.
+ */
+public class ReadMessage extends AbstractMessageTask {
+
+    public ReadMessage(AbstractMessageTask messageTask) {
+        super("read", "reads message subject");
+    }
+    
+
+    @Override
+    protected String executeOnMessage(ProgressStatusCallback callback, CachedMessage message) throws InterruptedException {
+        String subject = null;
+        try {
+            subject = message.getSubject();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        callback.update(50,subject);
+        return "";
+    }
+}
